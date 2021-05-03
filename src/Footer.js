196377afc81eb3bae5d9f972d2a5f20de23ab1ef
Yousef-Hma <yousef.hma@gmail.com>
2021-05-03 05:10:22 +0100

    project

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import { FaGithub } from 'react-icons/fa'
 const Footer = () => {
     return (
                 <div className="footer">
-                    <p style={{marginBottom:"0px"}}>&copy; 2021 CyberSecurity Educational Package! All content is publicly accessible on <a href="https://github.com/Yousef-Hma/Yousef-Hma-CyberSecurity-Online-Education-Package">GitHub <FaGithub /></a> and is subject to Copyright &copy;</p>
+                    <p style={{marginBottom:"0px"}}>&copy; 2021 CyberSecurity Educational Package! All content is publicly accessible on <a href="https://github.com/Yousef-Hma/Yousef-Hma-CyberSecurity-Online-Education-Package" target="_blank">GitHub <FaGithub /></a> and is subject to Copyright &copy;</p>
                     <p class="pull-right">
                         <a href="#top" id="back-top">
                             Back to Top				</a>
